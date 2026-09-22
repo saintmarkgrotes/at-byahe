a94aa1720d6ec9c69aa7f82d7248c9e041e00f61
unknown <grotessaintmark@gmail.com>
2026-09-22 09:49:18 +0800

    feat: remove mock data and add empty user constant

---
+++ src/constants/user.js
@@ -0,0 +1,5 @@
+// Who is using the app. There is no login or Profile screen yet, so type your first name here
+// if you want "Hello <name>," on Home. Leave it empty to show just "Hello,".
+export const user = {
+  firstName: '',
+};

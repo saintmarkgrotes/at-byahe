4e7ba058e35ef0d3c9d159adec05c8e34aeb6c81
unknown <grotessaintmark@gmail.com>
2026-09-22 09:49:32 +0800

    feat: add EmptyState component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,6 @@
 export { default as AppText } from './AppText';
 export { default as Card } from './Card';
+export { default as EmptyState } from './EmptyState';
 export { default as Pill } from './Pill';
 export { default as ProgressBar } from './ProgressBar';
 export { default as SectionHeader } from './SectionHeader';
